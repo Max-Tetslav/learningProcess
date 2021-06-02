d416e1fc4d1b64d2f12e6213192c3179f43743f4
Max Teclav <maks.tetslav@yandex.ru>
2021-06-02 12:41:50 +1000

    add autoplay

--- slider/main.js
+++ slider/main.js
@@ -1,3 +1,4 @@
+const slider = document.querySelector(".slider");
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 const slides = document.querySelectorAll(".slide");
@@ -48,3 +49,43 @@ prevBtn.addEventListener("click", () => {
     slideIcons[slideNumber].classList.add("active");
 
 });
+
+//autoplay
+
+var playSlider;
+
+var repeater = () => {
+    playSlider = setInterval(function() {
+            slides.forEach((slide) => {
+                slide.classList.remove("active");
+            });
+
+            slideIcons.forEach((slideIcon) => {
+                slideIcon.classList.remove("active");
+            })
+
+            slideNumber++;
+
+            if (slideNumber > (numbersOfSlides - 1)) {
+                slideNumber = 0;
+            }
+
+            slides[slideNumber].classList.add("active");
+            slideIcons[slideNumber].classList.add("active");
+        },
+        3000)
+}
+
+repeater();
+
+//stop autoplay on mouseover
+
+slider.addEventListener("mouseover", () => {
+    clearInterval(playSlider);
+});
+
+//start autoplay after mouseout
+
+slider.addEventListener("mouseout", () => {
+    repeater();
+})
